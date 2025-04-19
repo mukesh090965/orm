89f4c449d8c2f048ddb49fd02d5ae4aacef28d63
mukesh090965 <makprajapati333@gmail.com>
2025-04-19 14:37:02 +0530

    made a new router

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
 
-func main(){
-	r := http.NewRouter()
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "test")
+	})
+	http.ListenAndServe(":8080", r)
 }
